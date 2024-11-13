76a345f395ee831800b598e6546a93a975193cbd
sully-vian <vian.hervy@gmail.com>
2024-11-13 22:58:27 +0100

    formatted the ssh file script

--- .scripts/tmux-ssh.sh
+++ .scripts/tmux-ssh.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # custom ssh function with prompt if inside a tmux session
 ssh() {
   ORANGE="\033[0;33m"
@@ -7,11 +9,17 @@ ssh() {
   if [ -n "$TMUX" ]; then
     # prompt the user
     echo -e "${ORANGE}[WARNING]${RESET}\nYou are currently in a tmux session. The machine you're connecting to might automatically run a tmux session too, causing tmux session nesting."
-  read -p "Do you want to Proceed (y/n)?: " choice
+    read -p "Do you want to Proceed (y/n)?: " choice
     case "$choice" in
-      y|Y) echo "Proceeding with SSH...";;
-      n|N) echo "SSH aborted to avoid nested tmux sessions"; return 1;;
-      * ) echo "Invalid choice. SSH aborted."; return 1;;
+    y | Y) echo "Proceeding with SSH..." ;;
+    n | N)
+      echo "SSH aborted to avoid nested tmux sessions"
+      return 1
+      ;;
+    *)
+      echo "Invalid choice. SSH aborted."
+      return 1
+      ;;
     esac
   fi
   # execute the ssh command with the provided args

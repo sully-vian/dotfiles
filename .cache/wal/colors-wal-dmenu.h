cbf508bdfe3e3e428dd1e953ac35ad4ab3059db7
sully-vian <vian.hervy@gmail.com>
2025-02-27 00:31:57 +0100

    Added wal colors

---
+++ .cache/wal/colors-wal-dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbc4af", "#040F0A" },
+	[SchemeSel] = { "#bbc4af", "#1D5742" },
+	[SchemeOut] = { "#bbc4af", "#608B65" },
+};

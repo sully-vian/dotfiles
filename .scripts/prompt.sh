2a31e01138d8a1a58b875caacc84f67105521b44
Vianney HERVY <vian.hervy@free.fr>
2025-02-27 01:14:12 +0100

    Refactor prompt script: improve path handling, enhance prompt icons, and clean up code structure

--- .scripts/prompt.sh
+++ .scripts/prompt.sh
@@ -2,11 +2,11 @@
 
 # custom prompt
 set_prompt() {
-  
+
   # variables
   local BLUE="\[\e[34m\]"
   local WHITE="\[\e[0m\]"
-  
+
   local RED1="\[\e[1;31m\]"
   local RED0="\[\e[0;31m\]"
   local RED2="\[\e[2;31m\]"
@@ -18,59 +18,61 @@ set_prompt() {
   local BOLD="\[\e[1m\]"
   local RESET="\[\e[0m\]"
 
-  local arrow="➜"
-
   local lastcommand=$1
 
-    # get absolute path of git root
-  local  git_root_path=$(git rev-parse --show-toplevel 2>/dev/null)
+  # get absolute path of git root
+  local git_root_path
+  git_root_path=$(git rev-parse --show-toplevel 2>/dev/null)
 
-    if [ -n "$git_root_path" ]; then
-        # if inside git repo, display path from root of git repo
-        # git_root = git repo name
-        local git_root=$(basename "$git_root_path")
+  if [ -n "$git_root_path" ]; then
+    # if inside git repo, display path from root of git repo
+    # git_root = git repo name
+    local git_root
+    git_root=$(basename "$git_root_path")
 
-        if [ "$git_root_path" = "$PWD" ]; then
-            # if at root of git repo
-            local rel_path="󰊢 $git_root"
-        elif [ "$git_root" = "n7-files" ]; then
-            local rel_path="${BOLD}N7${RESET} ${RED0}${PWD#"$git_root_path/"}"
-        else
-            # just get the relative git filepath
-            local rel_path="📚 $git_root/${PWD#"$git_root_path/"}"
-        fi
+    if [ "$git_root_path" = "$PWD" ]; then
+      # if at root of git repo
+      local rel_path="󰊢 $git_root"
+    elif [ "$git_root" = "n7-files" ]; then
+      local rel_path="${BOLD}N7${RESET} ${RED0}${PWD#"$git_root_path/"}"
     else
-        case $PWD in
-        /) local rel_path="🪵" ;;
-        "$HOME"/.local/share/Trash*) local rel_path="🗑️ ${PWD#"$HOME/.local/share/Trash"}" ;;
-        "$HOME"/.config*) local rel_path=" ${PWD#"$HOME/.config"}" ;;
-        "$HOME"/Desktop*) local rel_path=" ${PWD#"$HOME/Desktop"}" ;;
-        "$HOME"/Downloads*) local rel_path="󰉍 ${PWD#"$HOME/Downloads"}" ;;
-        "$HOME"/Documents*) local rel_path="📄${PWD#"$HOME/Documents"}" ;;
-        "$HOME"/Music*) local rel_path="󱍙 ${PWD#"$HOME/Music"}" ;;
-        "$HOME"/Videos*) local rel_path="🎞️ ${PWD#"$HOME/Videos"}" ;;
-        "$HOME"/Pictures*) local rel_path=" ${PWD#"$HOME/Pictures"}" ;;
-        "$HOME"*) local rel_path="󰋜 ${PWD#"$HOME"}" ;;
-        *) local rel_path="📁 \w" ;;
-        esac
+      # just get the relative git filepath
+      local rel_path="󰊢 $git_root/${PWD#"$git_root_path/"}"
     fi
+  else
+    case $PWD in
+    "$HOME"/.local/share/Trash*) local rel_path="󰩹 ${PWD#"$HOME/.local/share/Trash"}" ;;
+    "$HOME"/.config*) local rel_path=" ${PWD#"$HOME/.config"}" ;;
+    "$HOME"/Desktop*) local rel_path=" ${PWD#"$HOME/Desktop"}" ;;
+    "$HOME"/Downloads*) local rel_path="󰉍 ${PWD#"$HOME/Downloads"}" ;;
+    "$HOME"/Music*) local rel_path="󱍙 ${PWD#"$HOME/Music"}" ;;
+    "$HOME"/Pictures*) local rel_path=" ${PWD#"$HOME/Pictures"}" ;;
+    "$HOME"*) local rel_path="󰋜 ${PWD#"$HOME"}" ;;
+    *) local rel_path="\w" ;;
+    esac
+  fi
 
-    PS1="┌[${BLUE}\u${WHITE}@\h:${RED0}$rel_path${WHITE}]\n└─"
-    PS1="$rel_path"
+  # PS1="$rel_path"
+  # colored path
+  PS1="${GREEN1}$rel_path${RESET}"
 
-    if [ "$lastcommand" -eq 0 ]; then
-        local coloredarrow="${GREEN1}$arrow ${RESET}"
-    else
-        local coloredarrow="${RED1}$arrow ${RESET}"
-    fi
+  local prompt_icon
+  if [ "$lastcommand" -eq 0 ]; then
+    # local coloredarrow="${GREEN1}$arrow ${RESET}"
+    prompt_icon="✔"
+  else
+    # local coloredarrow="${RED1}$arrow ${RESET}"
+    prompt_icon="✘"
+  fi
 
-    PS1="$PS1\n$coloredarrow"
+  # PS1="$PS1\n$coloredarrow"
+  PS1="$PS1\n$prompt_icon "
 
-    if [ -n "$SSH_CONNECTION" ]; then
-      PS1="[📡] $PS1"
-    fi
+  if [ -n "$SSH_CONNECTION" ]; then
+    PS1="󰣀 $PS1"
+  fi
 
-    if pgrep vpnc >/dev/null; then
-      PS1="[🌐] $PS1"
-    fi
+  if pgrep vpnc >/dev/null; then
+    PS1="󰖂 $PS1"
+  fi
 }

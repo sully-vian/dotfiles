a850e99f95db378c91cb2df4f3346f67d062e0d7
sully-vian <vian.hervy@gmail.com>
2025-02-27 01:17:41 +0100

    gitignore .cache/wal/

--- .cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#040F0A";
-static const char* selfgcolor   = "#bbc4af";
-static const char* normbgcolor  = "#275743";
-static const char* normfgcolor  = "#bbc4af";
-static const char* urgbgcolor   = "#1D5742";
-static const char* urgfgcolor   = "#bbc4af";

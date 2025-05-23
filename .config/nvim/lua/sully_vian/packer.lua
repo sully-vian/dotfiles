64f677f743e2f8ae2bcd76dfbc8388fff374e83d
sully-vian <vian.hervy@gmail.com>
2025-05-23 03:28:10 +0200

    début de l'aventure nvim

---
+++ .config/nvim/lua/sully_vian/packer.lua
@@ -0,0 +1,43 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use('wbthomason/packer.nvim')
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.8',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	-- use { 'AlphaTechnolog/pywal.nvim', as = 'pywal' }
+	use('tanvirtin/monokai.nvim')
+
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+	use('nvim-treesitter/playground') -- to see the syntax tree :TSPlaygroundToggle
+
+    use('neovim/nvim-lspconfig')
+
+    use {
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+    }
+
+    use {
+        'ray-x/lsp_signature.nvim',
+        config = function()
+            require("lsp_signature").setup({
+                bind = true,
+                handler_opts={
+                    boder="rounded"},
+                    floating_window=true,
+                    hint_table=false,
+                    floating_window_above_cur_line = true})
+                end
+            }
+end)
+

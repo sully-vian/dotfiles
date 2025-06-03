7bcec7b3b4190aa1768c24d9f8b6e5e974c4aa32
sully-vian <vian.hervy@gmail.com>
2025-06-03 09:56:51 +0200

    added nvim basic config, will prollycome back later

--- .config/nvim/lua/sully_vian/packer.lua
+++ .config/nvim/lua/sully_vian/packer.lua
@@ -4,21 +4,21 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use('wbthomason/packer.nvim')
+    -- Packer can manage itself
+    use('wbthomason/packer.nvim')
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-	-- use { 'AlphaTechnolog/pywal.nvim', as = 'pywal' }
-	use('tanvirtin/monokai.nvim')
+    -- use { 'AlphaTechnolog/pywal.nvim', as = 'pywal' }
+    use('tanvirtin/monokai.nvim')
 
-	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
 
-	use('nvim-treesitter/playground') -- to see the syntax tree :TSPlaygroundToggle
+    use('nvim-treesitter/playground') -- to see the syntax tree :TSPlaygroundToggle
 
     use('neovim/nvim-lspconfig')
 
@@ -32,12 +32,14 @@ return require('packer').startup(function(use)
         config = function()
             require("lsp_signature").setup({
                 bind = true,
-                handler_opts={
-                    boder="rounded"},
-                    floating_window=true,
-                    hint_table=false,
-                    floating_window_above_cur_line = true})
-                end
-            }
-end)
+                handler_opts = {
+                    boder = "rounded" },
+                floating_window = true,
+                hint_table = false,
+                floating_window_above_cur_line = true
+            })
+        end
+    }
 
+    use({'glepnir/lspsaga.nvim',after = 'nvim-lspconfig',config = function() require('lspsaga').setup({}) end)
+end)
--- .config/nvim/lua/sully_vian/remap.lua
+++ .config/nvim/lua/sully_vian/remap.lua
@@ -1,2 +1,3 @@
 vim.g.mapleader = " " -- leader key is space
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- in "n"ormal mode, leader+pv will execute cmd Ex (file exp)
+

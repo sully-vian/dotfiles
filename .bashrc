aef4ab433a78cbc47df5850c9906562408831390
Vianney HERVY <vian.hervy@free.fr>
2025-08-23 15:18:38 +0200

    adpated monitor setup for desktop and removed useless manpath in bashrc

--- .bashrc
+++ .bashrc
@@ -121,7 +121,6 @@ export NVM_DIR="$HOME/.config/nvm"
 
 # texlive path
 export PATH="/usr/local/texlive/2024/bin/x86_64-linux:$PATH"
-export MANPATH="/usr/local/texlive/2024/texmf-dist/doc/man:$MANPATH"
 export INFOPATH="/usr/local/texlive/2024/texmf-dist/doc/info:$INFOPATH"
 
 # opam init
--- .config/i3/monitor-setup.sh
+++ .config/i3/monitor-setup.sh
@@ -4,20 +4,27 @@
 # no monitor connected: laptop screen is primary and workspace 1.
 # monitor connected: monitor is primary and workspace 1.
 
-external_monitor="HDMI1"
-laptop_monitor="eDP1"
+hostname=$(hostname)
 
-if xrandr | grep "$external_monitor connected"; then
+if [[ "$hostname" == "archaic" ]]; then
+    right_monitor="eDP1"
+    left_monitor="HDMI1"
+elif [[ "$hostname" == "remy" ]]; then
+    right_monitor="VGA1"
+    left_monitor="DP1"
+fi
+
+if xrandr | grep "$left_monitor connected"; then
     # external monitor is connected
-    xrandr --output $external_monitor --primary --left-of $laptop_monitor --auto
-    i3-msg "workspace 1; move workspace to output $external_monitor"
-    i3-msg "workspace 2; move workspace to output $laptop_monitor"
+    xrandr --output $left_monitor --primary --left-of $right_monitor --auto
+    i3-msg "workspace 1; move workspace to output $left_monitor"
+    i3-msg "workspace 2; move workspace to output $right_monitor"
 else
     # only laptop monitor
-    xrandr --output $laptop_monitor --primary --auto
-    xrandr --output $external_monitor --off
+    xrandr --output $right_monitor --primary --auto
+    xrandr --output $left_monitor --off
     for i in {1..10}; do
-        i3-msg "workspace $i; move workspace to output $laptop_monitor" 2>/dev/null
+        i3-msg "workspace $i; move workspace to output $right_monitor" 2>/dev/null
     done
 fi
 

cf0ca04acbac77569d211f6fe2a9a5742490baa4
sully-vian <vian.hervy@gmail.com>
2025-09-25 22:13:30 +0200

    restarted nvim config from 0 (upcoming version 0.12)

--- .config/nvim/after/plugin/telescope.lua
+++
@@ -1,7 +0,0 @@
-local builtin = require('telescope.builtin')
-
-vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' }) -- find all files
-
-vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("find str in proj: ") });
-end)

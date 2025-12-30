52d6634b8de1bf868d23fb2fd7e8dbfa0cddec56
Vianney HERVY <vian.hervy@gmail.com>
2025-12-30 11:39:43 +0100

    added arduino, ocaml and php + small tweaks as live command and live-preview + exec file keymap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,6 +15,12 @@ vim.opt.winborder = "rounded"
 -- leader
 vim.g.mapleader = " "
 
+-- mappings
+local function exec_file()
+    local file = vim.fn.shellescape(vim.fn.expand("%:p"))
+    local output = vim.fn.system(file)
+    vim.print(output)
+end
 
 local function format()
     local view = vim.fn.winsaveview() -- save current view
@@ -47,24 +53,32 @@ local function format()
     vim.fn.winrestview(view) -- restore saved view
 end
 
--- mappings
-local function format_and_save()
-    format()
-    vim.cmd.write()
-end
-
 vim.keymap.set('n', "<leader>q", vim.cmd.quit, { desc = "Quit" })
-vim.keymap.set('n', "<leader>w", format_and_save, { desc = "Format+Save" })
+vim.keymap.set('n', "<leader>w", vim.cmd.write, { desc = "Save" })
 vim.keymap.set('n', "<leader>f", format, { desc = "Format buffer" })
 vim.keymap.set('n', "<leader>s", vim.cmd.source, { desc = "Source file" })
 vim.keymap.set('n', "<leader>r", vim.lsp.buf.rename, { desc = "Rename" })
 vim.keymap.set('n', "<leader>k", vim.lsp.buf.hover, { desc = "Hover" })
 vim.keymap.set('n', "<leader>d", vim.lsp.buf.definition, { desc = "Go to definition" })
 vim.keymap.set('n', "<leader>R", vim.lsp.buf.references, { desc = "List references" })
+vim.keymap.set('n', "<leader>e", exec_file, { desc = "execute file" })
 
 vim.keymap.set('n', "<leader>p", "<Cmd>:Pick files<CR>", { desc = "Pick file" })
 vim.keymap.set('n', "<leader>P", "<Cmd>:Pick grep_live<CR>", { desc = "Pick string" })
 vim.keymap.set('n', "<leader>h", "<Cmd>:Pick help<CR>", { desc = "Help" })
+
+
+-- interpret .env files as sh files
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = {
+        ".env",
+        ".env.*",
+    },
+    callback = function()
+        vim.bo.filetype = "sh"
+    end,
+})
+
 
 -- autocomplete
 vim.api.nvim_create_autocmd("LspAttach", {
@@ -81,24 +95,28 @@ vim.opt.completeopt:append("noselect")
 
 -- pack
 vim.pack.add({
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master" },
     { src = "https://github.com/nvim-mini/mini.pick" },
-    { src = "https://github.com/loctvl842/monokai-pro.nvim" },
     { src = "https://github.com/vague2k/vague.nvim" },
     { src = "https://github.com/folke/which-key.nvim" },
     { src = "https://github.com/wakatime/vim-wakatime" },
     { src = "https://github.com/chomosuke/typst-preview.nvim" },
+    { src = "https://github.com/smjonas/live-command.nvim" },
+    { src = "https://github.com/brianhuster/live-preview.nvim" }
 })
 vim.pack.update(nil, { force = true })
 
 -- colors
 vim.cmd("colorscheme vague")
 
-require "mini.pick".setup({})
+require "mini.pick".setup({ a = true })
 
 require("lsp")
 
-require("nvim-treesitter.configs").setup {
+require "live-command".setup({ commands = { Norm = { cmd = "norm" } } })
+vim.cmd("cnoreabbrev norm Norm")
+
+require "nvim-treesitter.configs".setup {
     ensure_installed = { "lua", "java", "html", "css", "javascript", "python", "vim", "typst" },
     auto_install = true,
     sync_install = false,

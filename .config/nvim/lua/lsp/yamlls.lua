c6731440a617131dd7633bfa6b93afb103442f75
sully-vian <vian.hervy@gmail.com>
2026-01-11 13:31:41 +0100

    NVIM: yamlls uses schemastore

--- .config/nvim/lua/lsp/yamlls.lua
+++ .config/nvim/lua/lsp/yamlls.lua
@@ -1,8 +1,16 @@
 vim.lsp.config("yamlls", {
-	cmd = { "yaml-language-server", "--stdio" },
-	filetypes = { "yaml", "yaml.docker-compose" },
-	root_markers = { "package.json", ".git" },
-	settings = { yaml = { format = { enable = true } } }
+    cmd = { "yaml-language-server", "--stdio" },
+    filetypes = { "yaml", "yaml.docker-compose" },
+    root_markers = { "package.json", ".git" },
+    settings = {
+        yaml = {
+            format = { enable = true },
+            schemaStore = {
+                enable = true,
+                url = "https://www.schemastore.org/api/json/catalog.json"
+            }
+        }
+    }
 })
 
 vim.lsp.enable("yamlls")

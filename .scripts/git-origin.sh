7a1b1b856cc2a2bdcd115b5c07a97fa824358bb6
sully-vian <62720515+sully-vian@users.noreply.github.com>
2026-04-24 10:20:36 +0200

    Refactor git-origin.sh for improved URL handling

--- .scripts/git-origin.sh
+++ .scripts/git-origin.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-
 # script to open github page from a repo in Tmux
 # strangly inspired by https://github.com/SylvanFranklin/.config/blob/main/scripts/open_github.sh
 
-cd $(tmux run "echo #{pane_current_path}") || exit 1
+cd "$(tmux display-message -p "#{pane_current_path}")" || exit 1
 
 git_root=$(git rev-parse --show-toplevel 2>/dev/null) || {
     tmux display-message "Not inside a git repository"
@@ -17,11 +16,17 @@ branch=$(git branch --show-current)
 
 url=$(git remote get-url origin)
 
+if [[ -z "$url" ]]; then
+    tmux display-message "No remote 'origin' found"
+    exit 1
+fi
+
 # if ssh, normalize to http:
 # git@hostname:user/repo.git -> https://hostname/user/repo
-if [[ $url =~ ^git@ ]]; then # git@github.com:<user>/<repo>
+if [[ $url =~ ^(ssh://)?git@ ]]; then # git@github.com:<user>/<repo>
+    url="${url#ssh://}" # remove ssh:// if it exists
 	url="${url#git@}" # remove git@
-	url="${url/:/\/}" # replace first ':' with '/'
+    url="${url/:/\/}" # replace first ':' with '/' (no-op for ssh:// urls)
 	url="https://$url" # add schema
 fi
 
@@ -29,7 +34,13 @@ url="${url%.git}" # remove trailing .git
 
 path_sep="tree" # some providers use other
 
-final_url="$url/$path_sep/$branch/$rel_path"
+if [[ -n "$branch" ]]; then # add branch
+    url="$url/$path_sep/$branch"
+fi
 
-tmux display-message -d 3000 "Opening $final_url..."
-xdg-open $final_url > /dev/null 2>&1 &
+if [[ -n "$rel_path" && "$rel_path" != "." ]]; then # add relative path
+    url="$url/$rel_path"
+fi
+
+tmux display-message -d 3000 "Opening $url..."
+xdg-open "$url" > /dev/null 2>&1 &

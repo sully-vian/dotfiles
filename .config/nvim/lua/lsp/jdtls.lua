bd03e3b7c7899f27375da90042ee62462cd35e62
sully-vian <vian.hervy@gmail.com>
2026-05-28 11:18:11 +0200

    NVIM: lombok support in jdtls

--- .config/nvim/lua/lsp/jdtls.lua
+++ .config/nvim/lua/lsp/jdtls.lua
@@ -19,8 +19,17 @@ local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
 -- path for .metadata , indexes (ref, gotodef, auto-completion) an project caches (build state)
 local workspace_dir = os.getenv('XDG_CACHE_HOME') .. '/jdtls/workspace/' .. project_name
 
+-- match all lombok-...<num>.jar (skip sources.jar and javadoc.jar)
+local lombok_paths = vim.fn.glob("~/.m2/repository/org/projectlombok/lombok/*/lombok-*[0-9].jar", true, true)
+local lombok_arg = (#lombok_paths > 0) and ("--jvm-arg=-javaagent:" .. lombok_paths[#lombok_paths]) or nil
+
 vim.lsp.config("jdtls", {
-    cmd = { "jdtls", "--java-executable", get_latest_java(), "-data", workspace_dir },
+    cmd = {
+        "jdtls",
+        "--java-executable", get_latest_java(),
+        "-data", workspace_dir,
+        lombok_arg
+    },
     filetypes = { "java" },
     root_markers = { "gradlew", "mvnw", "pom.xml", ".git" },
     init_options = {

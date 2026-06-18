f34ad17bdaff721d36d4df1921387565593f8ba2
sully-vian <vian.hervy@gmail.com>
2026-06-18 09:33:15 +0200

    NVIM: updated CSS handling in Biome, CSS LSP and TailwindCSS LSP

--- .config/nvim/lua/lsp/biome.lua
+++ .config/nvim/lua/lsp/biome.lua
@@ -1,7 +1,14 @@
 vim.lsp.config("biome", {
-	cmd = { "biome", "lsp-proxy" },
-	filetypes = { "css", "html", "javascript", "javascriptreact", "json", "jsonc", "typescript", "typescript.tsx", "typescriptreact" },
-	root_markers = { ".git", "biome.json", "package.json" },
+    cmd = { "biome", "lsp-proxy" },
+    filetypes = { "css", "html", "javascript", "javascriptreact", "json", "jsonc", "typescript", "typescript.tsx", "typescriptreact" },
+    root_markers = { ".git", "biome.json", "package.json" },
+    settings = {
+        css = {
+            parser = {
+                tailwindDirectives = true
+            }
+        }
+    }
 })
 
 vim.lsp.enable("biome")
--- .config/nvim/lua/lsp/css.lua
+++ .config/nvim/lua/lsp/css.lua
@@ -1,10 +1,16 @@
 vim.lsp.config("css", {
-	cmd = { "vscode-css-language-server", "--stdio" },
-	filetypes = { "css" },
-	root_markers = { "package.json", ".git" },
-	init_options = { provideFormatter = true },
-	settings = {
-		css = { validate = true } }
+    cmd = { "vscode-css-language-server", "--stdio" },
+    filetypes = { "css" },
+    root_markers = { "package.json", ".git" },
+    init_options = { provideFormatter = true },
+    settings = {
+        css = {
+            validate = true,
+            lint = {
+                --unknownAtRules = "ignore"
+            }
+        }
+    }
 })
 
 vim.lsp.enable("css")

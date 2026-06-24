96ada9b9508c74f6f2b1b701d1c98d0ddeadb061
sully-vian <vian.hervy@gmail.com>
2026-06-24 09:33:57 +0200

    NVIM: added twig as filetype for HTML lsp

--- .config/nvim/lua/lsp/html.lua
+++ .config/nvim/lua/lsp/html.lua
@@ -1,12 +1,12 @@
 vim.lsp.config("html", {
-	cmd = { "vscode-html-language-server", "--stdio" },
-	filetypes = { "html", "svg" },
-	root_markers = { "package.json", ".git" },
-	init_options = {
-		provideFormatter = true,
-		embeddedLanguages = { css = true, javascript = true },
-		configurationSection = { "html", "css", "javascript" }
-	}
+    cmd = { "vscode-html-language-server", "--stdio" },
+    filetypes = { "html", "svg", "twig" },
+    root_markers = { "package.json", ".git" },
+    init_options = {
+        provideFormatter = true,
+        embeddedLanguages = { css = true, javascript = true },
+        configurationSection = { "html", "css", "javascript" }
+    }
 })
 
 vim.lsp.enable("html")

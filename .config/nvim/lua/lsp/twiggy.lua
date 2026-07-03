a65460fdd87afee7b0c93c7a168096f1b5bcd198
sully-vian <vian.hervy@gmail.com>
2026-07-03 09:30:48 +0200

    NVIM: added better config to twiggy

--- .config/nvim/lua/lsp/twiggy.lua
+++ .config/nvim/lua/lsp/twiggy.lua
@@ -2,5 +2,13 @@ vim.lsp.config("twiggy", {
     cmd = { "twiggy-language-server", "--stdio" },
     filetypes = { "twig" },
     root_markers = { "composer.json", ".git" },
+    settings = {
+        twiggy = {
+            phpExecutable = "php",
+            symfonyConsolePath = "bin/console",
+            framwork = "symfony",
+            diagnostics = { twigCsFixer = true }
+        }
+    }
 })
 vim.lsp.enable("twiggy")

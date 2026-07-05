dab9938fe91bb06824ae1b91e8bed932101a2267
sully-vian <vian.hervy@gmail.com>
2026-07-05 21:29:48 +0200

    NVIM: checked config with strict types

--- .config/nvim/lua/lsp/lua.lua
+++ .config/nvim/lua/lsp/lua.lua
@@ -1,7 +1,7 @@
 vim.lsp.config("lua-language-server", {
     cmd = { "lua-language-server" },
     filetypes = { "lua" },
-    root_markers = { ".luarc.json", ".git" },
+    root_markers = { ".luarc.json", ".luarc.jsonc", ".git" },
 })
 
 vim.lsp.enable("lua-language-server")

bab3110b8664b59092465b9ae5659197443efa10
sully-vian <vian.hervy@gmail.com>
2026-07-07 10:25:18 +0200

    NVIM: added `.phpactor.json` as root marker for phpactor

--- .config/nvim/lua/lsp/phpactor.lua
+++ .config/nvim/lua/lsp/phpactor.lua
@@ -1,7 +1,7 @@
 vim.lsp.config("phpactor", {
     cmd = { "phpactor", "language-server" },
     filetypes = { "php" },
-    root_markers = { '.git', "composer.json", ".phpactor.yml", "phpactor.json" },
+    root_markers = { ".phpactor.json", "composer.json", ".phpactor.yml", "phpactor.json", ".git" },
     workspace_required = true,
 })
 vim.lsp.enable("phpactor")

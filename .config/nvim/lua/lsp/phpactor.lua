39b7568e4da64ac07a077e1bf06a82d60b3dc325
sully-vian <vian.hervy@gmail.com>
2026-07-23 10:18:30 +0200

    NVIM: added `gotoRelevantDef` to `phpactor` config

--- .config/nvim/lua/lsp/phpactor.lua
+++ .config/nvim/lua/lsp/phpactor.lua
@@ -1,7 +1,27 @@
+local function gotoRelevantDef()
+    vim.lsp.buf.definition({
+        on_list = function(options)
+            local items = options.items
+            if #items > 1 then
+                items = vim.tbl_filter(function(item) -- keep only vendor
+                    return string.match(item.filename, "/vendor/")
+                end, items)
+            end
+            if #items == 0 then items = options.items end
+            vim.fn.setqflist({}, ' ', { title = options.title, items = items })
+            vim.api.nvim_command("cfirst")
+        end
+    })
+end
+
 vim.lsp.config("phpactor", {
     cmd = { "phpactor", "language-server" },
     filetypes = { "php" },
     root_markers = { ".phpactor.json", "composer.json", ".phpactor.yml", "phpactor.json", ".git" },
     workspace_required = true,
+    on_attach = function(_, bufnr)
+        vim.keymap.set('n', "<leader>d", gotoRelevantDef, { buffer = bufnr, desc = "Go to relevant definition" })
+    end
 })
+
 vim.lsp.enable("phpactor")

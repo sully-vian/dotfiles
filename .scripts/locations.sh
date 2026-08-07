397e23083895fc126466769cd1cc04ee3b31e832
sully-vian <vian.hervy@gmail.com>
2026-08-07 09:39:05 +0200

    VIM: moved `.tmux.conf` to `.config/tmux/tmux.conf` and updated references

--- .scripts/locations.sh
+++ .scripts/locations.sh
@@ -2,7 +2,7 @@
 export DOTFILES="$HOME/dotfiles/"
 export BASHRC="$HOME/.bashrc"
 export ALIASES="$HOME/.scripts/aliases.sh"
-export TMUX_CONF="$HOME/.tmux.conf"
+export TMUX_CONF="$HOME/.config/tmux/tmux.conf"
 export VIMRC="$HOME/.config/vim/vimrc"
 
 export LOCATIONS=$BASHRC:$ALIASES:$TMUX_CONF:$VIMRC
